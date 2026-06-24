918e0c7019ee3335b2658bfd5b95e4a4b1820509
jinwooshin-cpu <jinwoo.shin@tbnws.com>
2026-06-24 16:53:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// 우유 미션 게임 - 서비스 워커 (홈화면 추가 / 기본 오프라인)
+const CACHE = 'milk-mission-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  // Firebase/구글폰트 등 외부 요청은 항상 네트워크로 (캐시하지 않음)
+  if (url.origin !== self.location.origin) return;
+  // 앱 셸: 네트워크 우선, 실패 시 캐시
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
